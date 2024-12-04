0ea4dc4c7851b1ef4f0eb60ebb02ab964688cdea
João Périco <joaoeperico@gmail.com>
2024-12-04 16:17:53 -0300

    [feat] add swagger

---
+++ Makefile
@@ -0,0 +1,11 @@
+runserver:
+	python3 server/manage.py runserver
+
+migrate:
+	python3 server/manage.py migrate
+
+makemigrations:
+	python3 server/manage.py makemigrations
+
+createsuperuser:
+	python3 server/manage.py createsuperuser
---
+++ server/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from drf_spectacular.views import (
+  SpectacularAPIView,
+  SpectacularRedocView,
+  SpectacularSwaggerView,
+)
+
+urlpatterns = [
+  path('schema/', SpectacularAPIView.as_view(), name='schema'),
+  path(
+    'schema/swagger/',
+    SpectacularSwaggerView.as_view(url_name='schema'),
+    name='swagger-ui'
+  ),
+]
